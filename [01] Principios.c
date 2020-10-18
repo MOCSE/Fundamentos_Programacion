6980c19280463232c4381622e5bf45589ae9560d
dfg161 <66406782+dfg161@users.noreply.github.com>
2020-10-18 14:29:41 -0500

    version 1.0

---
+++ [01] Principios.c
@@ -0,0 +1,23 @@
+//Lenguaje C
+
+//Archivos de Biblioteca (.h significa cabecera de 'libreria').
+//Directiva del preprocesador (# 'Numeral') indicando que va incluir un archivo de biblioteca.
+
+#include <stdio.h> //standard input-output header | Contiene las definiciones de las macros, las constantes, las declaraciones de funciones de la biblioteca estándar
+#include <stdlib.h> //standard library | Contiene los prototipos de funciones de C para gestión de memoria dinámica, control de procesos y otras.
+
+main() //Funcion principal - Si se agrega return 0 "int main ()"
+{
+	system("COLOR 65"); //Color Consola: (“color de fondo” “color de letra”);
+	
+	printf("Hola Mundo \nParrafo2\n"); //Impresión De Pantalla con formato.
+	// "\n" salto de linea
+	//Sentencia: debe terminar la mayoria de veces con ";"
+	
+	system("PAUSE"); //Presione una tecla para continuar - Pausa del programa.
+		
+} //Fin de programa en C {}
+
+//Compilar y Ejecturar "F11"
+
+
---
+++ [02] Tipo de Datos & Operadores.cpp
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define PI 3.1416; //Definir Constantes (MAYUSCULA).
+
+main()
+{
+	
+	//Constantes (MAYUSCULAS)
+	const float EULER = 2.71828; //Definir Decimales Constantes.
+	const int R = 25;	         //Definir Enteros Constantes.
+	
+	//Variables (minusculas)
+	int z = 12;				     		 //Definir Enteros.
+	char letra;				             //Definir Caracter.
+	float decimal = 9.323454;		 	 //Definir Decimal sencillo (7 digitos).
+	double decimal2 = 9.32123456432121;	 //Definir Decimal doble (15 digitos).
+	
+	//Operadores
+		//Suma +
+		//Resta -
+		//Porducto *
+		//Cociente /
+		//Modulo % (100%3=1) - Residuo de division
+	
+	//Operadores relacionales
+		//Mayor que >
+		//Menor que <
+		//Mayor o igual que >=
+		//Menor o igual que <=
+		//Igual que ==
+		//Diferente de !=
+		
+			//Resultados Falso o Verdadero - 0 o 1
+			
+	//Operadores Logicos
+		//And && (Binario)
+		//Or  || (Binario)
+		//Not !  (Unario)
+		
+	//Operadores Unarios
+		//Not !
+		//Incremento ++
+			//Post incremento x++
+			//Pre incremento ++x
+		//Decremento --
+			//Post Decremento x--
+			//Pre Decremento --x
+			
+		//Ejemplo de Incremento
+	printf("-------------------\n");
+		int x2 = 3;
+		printf("x2 = %d \n",x2);
+		x2++;
+		printf("x2 (++) = %d \n",x2);
+	
+	printf("-------------------\n");
+	
+		//Ejemplo de Pre-Incremento
+		int x,y;
+		x=3; y=x;
+		printf("x=%d	y=%d\n",x,y);
+		x=++y;
+		printf("x=%d	y=%d\n",x,y);
+	
+	printf("-------------------n");
+		
+	//Operador Ternario
+		// ? : | op1?op2:op3
+			//Si es verdadero el resultado es el op2
+			//Si es falso se el resultado es el op3
+
+			//Ejemplo Operador Ternario
+			int z2;
+			z2=(2==3)?10:20;
+			printf("z2 = %d",z2);
+	
+	
+}
