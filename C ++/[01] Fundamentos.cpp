f3a2a965d4cb41ebb210d0cf4fd0b29405be18ea
dfg161 <66406782+dfg161@users.noreply.github.com>
2020-10-20 02:27:12 -0500

    1-2 De C++

---
+++ C ++/[01] Fundamentos.cpp
@@ -0,0 +1,25 @@
+//Lenguaje C++
+
+
+#include <iostream> //Libreria de entrada y salida de datos <3
+
+using namespace std; //Estandar de c++ | Flujos de entrada y salida fast
+
+
+int main(){
+	
+	cout<<"Este es un espacio en pantalla :O"<<endl; //Espacio en pantalla
+	cout<<"Hola Mundo :D \n"; //Imprimir en pantalla
+	
+		int entero = 15;
+		float flotante = 12.33;
+		double doble = 10.12312;
+		char cara = 'a';
+		
+		//Imprimir y concatenar
+		cout<<"Entero: "<< entero << "| Flotante: " << flotante << "| Doble: " << doble << "| Char: " << cara <<endl;
+	
+	return 0; //Buenas practicas de programación, YEIIIIII
+}
+
+
---
+++ C ++/[02] Entrada de datos.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <math.h> //Libreria operadores como raiz o exponentes 
+
+using namespace std; 
+
+int main(){
+	cout.precision(3); //Numeros a mostrar en pantalla
+	
+	
+	//1. Escribe la siguiente función como expresión en C++ (a/b)+1
+	cout<<"Programa 1"<<endl;
+	
+	float A,B,C;
+	
+	cout << "Ingrese A: "; cin >> A; //Leer datos de entrada
+	cout << "Ingrese B: "; cin >> B;
+	
+	C=(A/B)+1; 
+	
+	cout <<"Tu resultado Es: " <<C<<endl<<endl;
+	
+		
+		
+		system("pause"); //Presione una tecla para continuar
+		system("cls"); //Limpia la consola
+		
+		
+		
+		
+	//2. Escribe la siguiente función como expresión en C++ f(x,y) = Raiz(x) / y^2-1
+	cout<<"Programa 2"<<endl;
+	
+	float x,y,z;
+	
+	cout << "Ingrese X: "; cin >> x;
+	cout << "Ingrese Y: "; cin >> y;
+	
+	//Sqrt(x) -> Raiz || pow(x,^) -> Exponente
+	z = ((sqrt(x)) / (pow(y,2)-1));
+	
+	cout <<"El resultado de la Funcion es: " <<z<<endl<<endl;
+	
+	return 0; 
+}
+
